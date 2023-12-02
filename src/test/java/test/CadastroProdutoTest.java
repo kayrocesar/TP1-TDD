999b3bcf51e856a5b8f1a607de9112262100f09d
kayrocesar <kayro.cesar.kc@gmail.com>
2023-12-02 17:23:42 -0300

    add teste cadastro produto por triangulação
    Co-Authored-By: Lucas <lucasgabriel_df@hotmail.com>
    Co-Authored-By: Luan Cavalcante <67024690+Luan-Cavalcante@users.noreply.github.com>

--- src/test/java/test/CadastroProdutoTest.java
+++ src/test/java/test/CadastroProdutoTest.java
@@ -39,7 +39,7 @@ public class CadastroProdutoTest {
         assertEquals(1,sistemaEstoque.getProdutos().size());
     }
     @Test
-    public void cadastrarDoisProdutoTest() throws DescricaoEmBrancoException {
+    public void cadastrarDoisProdutosTest() throws DescricaoEmBrancoException {
         Produto produto1 = new Produto("Bolo de chocolate",
                 "Bolo de chocolate (1kg) com cobertura de morango",
                 "123456789",
@@ -66,6 +66,47 @@ public class CadastroProdutoTest {
         sistemaEstoque.cadastrarProduto(produto1);
         sistemaEstoque.cadastrarProduto(produto2);
         assertEquals(2,sistemaEstoque.getProdutos().size());
+    }
+    @Test
+    public void cadastrarTresProdutosTest() throws DescricaoEmBrancoException {
+        Produto produto1 = new Produto("Bolo de chocolate",
+                "Bolo de chocolate (1kg) com cobertura de morango",
+                "123456789",
+                10.0,
+                25.0,
+                10,
+                5,
+                LocalDate.of(2024,8,5),
+                1,
+                Categoria.BOLO,
+                new Fornecedor("Bolos da dona maria LTDA", "42.679.040/0001-51"));
+        Produto produto2 = new Produto("Salgado ",
+                "coxinha",
+                "123456700",
+                5.0,
+                18.0,
+                100,
+                20,
+                LocalDate.of(2024,8,5),
+                1,
+                Categoria.SALGADO,
+                new Fornecedor("Salgados do seu Zé SA", "40.600.040/0001-51"));
+        Produto produto3 = new Produto("Brigadeiro",
+                "Brigadeiro gourmet com chocolate 60% cacau",
+                "123456100",
+                3.0,
+                10.0,
+                200,
+                50,
+                LocalDate.of(2024,1,5),
+                1,
+                Categoria.DOCE,
+                new Fornecedor("Doces do seu João INC", "41.100.040/0001-61"));
+
+        sistemaEstoque.cadastrarProduto(produto1);
+        sistemaEstoque.cadastrarProduto(produto2);
+        sistemaEstoque.cadastrarProduto(produto3);
+        assertEquals(3,sistemaEstoque.getProdutos().size());
     }
 
 
