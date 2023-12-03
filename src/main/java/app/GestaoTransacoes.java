ec6dc7aaa50d80c8b8b93c5502902bbb4c92d22c
AdneMoretti <morettiadne1@gmail.com>
2023-12-02 22:42:12 -0300

    Implementando duplicacao com falha para transacao
    Co-authored-by: Gabriel Moretti <bielmoreti@gmail.com>

--- src/main/java/app/GestaoTransacoes.java
+++ src/main/java/app/GestaoTransacoes.java
@@ -1,18 +1,28 @@
 package app;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 
+import enums.TipoTransacao;
+import model.Produto;
 import model.Transacao;
 
 public class GestaoTransacoes {
 	
 	private ArrayList<Transacao> transacoes; 
 	
+	public GestaoTransacoes() {
+		this.transacoes = new ArrayList<>(); 
+		
+	}
+	
     public int getQuantidadeTransacao() {
-		return 1; 
+		return this.transacoes.size(); 
     }
 
-    public void cadastrarTransacao() {
-    	
+    public void cadastrarTransacao(LocalDate data, Produto produto, TipoTransacao tipoTransacao) {
+    	Transacao transacao = new Transacao(data, produto, tipoTransacao); 
+    	this.transacoes.add(transacao); 
     }
+   
 }
--- src/test/java/test/CadastroTransacaoTest.java
+++ src/test/java/test/CadastroTransacaoTest.java
@@ -1,11 +1,17 @@
 package test;
 
 import app.GestaoTransacoes;
+import enums.*;
+import model.Fornecedor;
+import model.Produto;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.time.LocalDate;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import java.time.LocalDate;
 
 class CadastroTransacaoTest {
 	
@@ -18,9 +24,38 @@ class CadastroTransacaoTest {
 		
 	@Test
 	public void testCadastroTransacao() { 
-		gestao.cadastrarTransacao(); 
-		System.out.print(gestao.getQuantidadeTransacao());
+        Produto produto1 = new Produto("Coca cola",
+                "Coca cola de 2 litros sem acucar",
+               "271462742",
+               20.0,
+               10.0,
+               50,
+               2,
+               LocalDate.of(2023,12,1),
+               1,
+               Categoria.REFRIGERANTE,
+               new Fornecedor("Brasal Refrigerantes LTDA", "42.679.040/0001-51"));
+        LocalDate date = LocalDate.now(); 
+		gestao.cadastrarTransacao(date, produto1, TipoTransacao.VENDA); 
 		assertEquals(gestao.getQuantidadeTransacao(), 1); 
+	}
+	
+	@Test
+	public void testCadastroDuasTransacao() { 
+        Produto produto1 = new Produto("Iogurte Grego",
+                "Iogurte Grego de Mousse de Limão",
+               "241242422",
+               2.50,
+               4.0,
+               30,
+               1,
+               LocalDate.of(2023,12,22),
+               1,
+               Categoria.IOGURTE,
+               new Fornecedor("ITALAC LTDA", "42.679.040/0001-51"));
+        LocalDate date = LocalDate.now(); 
+		gestao.cadastrarTransacao(date, produto1, TipoTransacao.VENDA); 
+		assertEquals(gestao.getQuantidadeTransacao(), 2); 
 	}
 
 }
