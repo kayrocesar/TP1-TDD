c769469bc34146c6599c693183d8b5c420ccd320
AdneMoretti <morettiadne1@gmail.com>
2023-12-02 23:45:31 -0300

    Corrigindo teste duplicado
    Co-authored-by: Gabriel Moretti <bielmoreti@gmail.com>

--- src/test/java/test/CadastroTransacaoTest.java
+++ src/test/java/test/CadastroTransacaoTest.java
@@ -4,10 +4,13 @@ import app.GestaoTransacoes;
 import enums.*;
 import model.Fornecedor;
 import model.Produto;
+import model.ProdutoQuantidade; 
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -35,14 +38,28 @@ class CadastroTransacaoTest {
                1,
                Categoria.REFRIGERANTE,
                new Fornecedor("Brasal Refrigerantes LTDA", "42.679.040/0001-51"));
+        ProdutoQuantidade produtoQuantidade = new ProdutoQuantidade(produto1, 2); 
+        ArrayList<ProdutoQuantidade> produtos = new ArrayList<> ();  
+        produtos.add(produtoQuantidade);
         LocalDate date = LocalDate.now(); 
-		gestao.cadastrarTransacao(date, produto1, TipoTransacao.VENDA); 
+		gestao.cadastrarTransacao(date, produtos, TipoTransacao.VENDA); 
 		assertEquals(gestao.getQuantidadeTransacao(), 1); 
 	}
 	
 	@Test
 	public void testCadastroDuasTransacao() { 
-        Produto produto1 = new Produto("Iogurte Grego",
+        Produto produto1 = new Produto("Coca cola",
+                "Coca cola de 2 litros sem acucar",
+               "271462742",
+               20.0,
+               10.0,
+               50,
+               2,
+               LocalDate.of(2023,12,1),
+               1,
+               Categoria.REFRIGERANTE,
+               new Fornecedor("Brasal Refrigerantes LTDA", "42.679.040/0001-51"));
+        Produto produto2 = new Produto("Iogurte Grego",
                 "Iogurte Grego de Mousse de Limão",
                "241242422",
                2.50,
@@ -53,8 +70,13 @@ class CadastroTransacaoTest {
                1,
                Categoria.IOGURTE,
                new Fornecedor("ITALAC LTDA", "42.679.040/0001-51"));
+        ProdutoQuantidade produtoQuantidade = new ProdutoQuantidade(produto1, 2); 
+        ProdutoQuantidade produtoQuantidade2 = new ProdutoQuantidade(produto2, 10); 
+        ArrayList<ProdutoQuantidade> produtos = new ArrayList<> ();  
+        produtos.add(produtoQuantidade);
+        produtos.add(produtoQuantidade2);
         LocalDate date = LocalDate.now(); 
-		gestao.cadastrarTransacao(date, produto1, TipoTransacao.VENDA); 
+		gestao.cadastrarTransacao(date, produtos, TipoTransacao.VENDA); 
 		assertEquals(gestao.getQuantidadeTransacao(), 2); 
 	}
 
